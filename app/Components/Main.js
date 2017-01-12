be9df6afa447f1205176942827cb866ea3ae30b2
RJRowland93 <r.rowland93@gmail.com>
2017-01-12 11:20:19 -0600

    added search history component

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -4,7 +4,7 @@ var React = require("react");
 // Here we include all of the sub-components
 var Form = require("./children/Form");
 var Results = require("./children/Results");
-
+var History = require("./children/History");
 // Helper Function
 var helpers = require("./utils/helpers.js");
 
@@ -63,6 +63,12 @@ var Main = React.createClass({
           <div className="col-md-6">
 
             <Results address={this.state.results} />
+
+          </div>
+
+          <div className="col-md-12">
+            
+           <History />
 
           </div>
 
